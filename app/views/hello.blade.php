5897aec40314d32fa61f851e65293e08f6fddc03
Mark Innes <innesm4@gmail.com>
2014-06-22 12:40:29 +0100

    added admin tools to home screen

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -15,50 +15,64 @@ Volunteer
         </div>
     </div>   
     
-	@if (Sentry::check() && Sentry::getUser()->hasAccess('users'))
+	
      <div class="row">
         <div class="col-lg-6">
-            <h3>My Profile</h3>
-            <hr>
-            <h3>My Skills</h3>
-            @foreach($skills as $skill)
-                @if(isset($assigned))
-                 <label class="checkbox">
-                    {{ Form::checkbox(
-                        'skills[]', 
-                        $skill->id, 
-                        in_array($skill->id, $assigned), ['disabled' => 'disabled']) 
-                    }}{{ $skill->skill_name }}</label>
-                @else
-                 <label class="checkbox">
-                    {{ Form::checkbox(
-                        'skills[]', 
-                        $skill->id, ['disabled' => 'disabled']) 
-                    }}{{ $skill->skill_name }}</label>
-                @endif
-            @endforeach
+            @if (Sentry::check() && Sentry::getUser()->hasAccess('users'))
+                <h3>My Profile</h3>
+                <hr>
+                <h3>My Skills</h3>
+                @foreach($skills as $skill)
+                    @if(isset($assigned))
+                     <label class="checkbox">
+                        {{ Form::checkbox(
+                            'skills[]', 
+                            $skill->id, 
+                            in_array($skill->id, $assigned), ['disabled' => 'disabled']) 
+                        }}{{ $skill->skill_name }}</label>
+                    @else
+                     <label class="checkbox">
+                        {{ Form::checkbox(
+                            'skills[]', 
+                            $skill->id, ['disabled' => 'disabled']) 
+                        }}{{ $skill->skill_name }}</label>
+                    @endif
+                @endforeach
 
-            <h3>Locations I am available for:</h3>
-            @foreach($locations as $location)
-                @if(isset($location_assigned))
-                 <label class="checkbox">
-                    {{ Form::checkbox(
-                        'locations[]', 
-                        $location->id, 
-                        in_array($location->id, $location_assigned), ['disabled' => 'disabled']) 
-                    }}{{ $location->location_name }}</label>
-                @else
-                 <label class="checkbox">
-                    {{ Form::checkbox(
-                        'locations[]', 
-                        $location->id,['disabled' => 'disabled']) 
-                    }}{{ $location->location_name }}</label>
-                @endif
-            @endforeach
-            <a href="/users/edit/{{{ Sentry::getUser()->id }}}" class="btn btn-md btn-primary">Edit Profile</a>
+                <h3>Locations I am available for:</h3>
+                @foreach($locations as $location)
+                    @if(isset($location_assigned))
+                     <label class="checkbox">
+                        {{ Form::checkbox(
+                            'locations[]', 
+                            $location->id, 
+                            in_array($location->id, $location_assigned), ['disabled' => 'disabled']) 
+                        }}{{ $location->location_name }}</label>
+                    @else
+                     <label class="checkbox">
+                        {{ Form::checkbox(
+                            'locations[]', 
+                            $location->id,['disabled' => 'disabled']) 
+                        }}{{ $location->location_name }}</label>
+                    @endif
+                @endforeach
+                <a href="/users/edit/{{{ Sentry::getUser()->id }}}" class="btn btn-md btn-primary">Edit Profile</a>
+
+            @endif 
+
+            @if (Sentry::check() && Sentry::getUser()->hasAccess('admin'))
+            <h3>Admin Tools</h3>
+            <hr>
+            <p><a href="/opportunities/create" class="btn btn-md btn-primary">Add Volunteering Opportunity</a></p>
+            <p><a href="/skills/create" class="btn btn-md btn-primary">Add Skill</a></p>
+            <p><a href="/locations/create" class="btn btn-md btn-primary">Add Location</a></p>
+            <p><a href="/community_events/create" class="btn btn-md btn-primary">Add Community Event</a></p>
+
+            @endif 
  
-        </div>
+        </div> 
         <div class="col-lg-6">
+            @if (Sentry::check() && Sentry::getUser()->hasAccess('users'))
             <h3>Volunteer Matches</h3>
             <table class="table table-striped">
                 <thead>
@@ -90,6 +104,7 @@ Volunteer
                     @endforeach
               </tbody>
             </table>
+            @endif 
             <h3>Latest Community Events</h3>
             <table class="table table-striped">
                 <thead>
@@ -117,6 +132,4 @@ Volunteer
 
         
     </div>         
-
-    @endif  
 @stop
